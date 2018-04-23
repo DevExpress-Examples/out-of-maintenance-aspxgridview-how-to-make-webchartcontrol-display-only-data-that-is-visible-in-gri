8a587b689de2e0f069a56c193ea58633a645dd4c
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Default.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using DevExpress.Xpo;
+using System.Collections.Generic;
+
+public partial class _Default : System.Web.UI.Page {
+    protected Session session1;
+
+    protected void Page_Init(object sender, EventArgs e) {
+        session1 = new Session();
+        XpoDataSource1.Session = session1;
+    }
+
+    protected void Page_PreRender(object sender, EventArgs e) {
+        List<nwind.Products> list = new List<nwind.Products>();
+        for (int i = ASPxGridView1.VisibleStartIndex; i < ASPxGridView1.SettingsPager.PageSize + ASPxGridView1.VisibleStartIndex; i++) {
+            if (!ASPxGridView1.IsGroupRow(i))
+                list.Add((nwind.Products)ASPxGridView1.GetRow(i));
+        }
+        WebChartControl1.DataSource = list;
+        WebChartControl1.DataBind();
+    }
+
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# ASPxGridView - How to make WebChartControl display only data that is visible in grid
+
+
+<p>This example demonstrates how to make WebChartControl display only data that is visible in grid. Note that ASPxGridView in this example is working in PostBack mode in order to make possible update of the chart. If you want to avoid refreshing of the whole page, <a href="https://www.devexpress.com/Support/Center/p/T552552">it is possible to place controls in UpdatePanel.</a><br><br>See also: <a href="https://www.devexpress.com/Support/Center/p/E1277">How to update a WebChartControl according to the changes made to the ASPxGridView</a> </p>
+
+<br/>
+
+
