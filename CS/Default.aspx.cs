8ca610786dc7510462d576d9b4a1d5e8e1f4a11e
Roman Balaban <25555034+onebalaban@users.noreply.github.com>
2018-08-16 15:25:18 +0300

    Fixed application logic to display changes in a web chart if ASPxGridView is filtered.

--- CS/Default.aspx.cs
+++ CS/Default.aspx.cs
@@ -14,18 +14,29 @@ public partial class _Default : System.Web.UI.Page {
     protected Session session1;
 
     protected void Page_Init(object sender, EventArgs e) {
-        session1 = new Session();
-        XpoDataSource1.Session = session1;
+        ASPxGridView1.JSProperties["cpCallbackName"] = String.Empty;
     }
-
     protected void Page_PreRender(object sender, EventArgs e) {
-        List<nwind.Products> list = new List<nwind.Products>();
-        for (int i = ASPxGridView1.VisibleStartIndex; i < ASPxGridView1.SettingsPager.PageSize + ASPxGridView1.VisibleStartIndex; i++) {
-            if (!ASPxGridView1.IsGroupRow(i))
-                list.Add((nwind.Products)ASPxGridView1.GetRow(i));
+        if(!IsPostBack) {
+            Session["DataSource"] = AccessDataSource1;
         }
+        if(Session["DataSource"] is AccessDataSource)
+            WebChartControl1.DataSource = (AccessDataSource)Session["DataSource"];
+        else
+            WebChartControl1.DataSource = (List<DataRowView>)Session["DataSource"];
+        WebChartControl1.DataBind();
+    }
+    protected void ASPxGridView1_AfterPerformCallback(object sender, DevExpress.Web.ASPxGridViewAfterPerformCallbackEventArgs e) {
+        ASPxGridView1.JSProperties["cpCallbackName"] = e.CallbackName;
+    }
+    protected void WebChartControl1_CustomCallback(object sender, DevExpress.XtraCharts.Web.CustomCallbackEventArgs e) {
+        var list = new List<DataRowView>();
+        for(int i = 0; i < ASPxGridView1.VisibleRowCount; i++) {
+            if(!ASPxGridView1.IsGroupRow(i))
+                list.Add((DataRowView)ASPxGridView1.GetRow(i));
+        }
+        Session["DataSource"] = list;
         WebChartControl1.DataSource = list;
         WebChartControl1.DataBind();
     }
-
 }
